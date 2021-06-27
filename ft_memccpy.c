9a332c3fc213aba49af6b7ce0d34d97bfccb7c10
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2021-06-27 16:32:19 -0300

    commit

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,29 +6,25 @@
 /*   By: akenji-a <akenji-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 14:14:14 by akenji-a          #+#    #+#             */
-/*   Updated: 2021/06/27 16:29:07 by akenji-a         ###   ########.fr       */
+/*   Updated: 2021/06/27 16:32:05 by akenji-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy (void *dest, const void *src, int c, size_t len)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t len)
 {
-	size_t	i;
+	size_t	x;
 
-	i = 0;
+	x = 0;
 	if (!dest && !src)
 		return (0);
-	while (i < len)
+	while (x < n)
 	{
-		if (c != ((unsigned char *)src)[i])
-			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-		else
-		{
-			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-			return (dest);
-		}
-		i++;
+		((unsigned char *)dest)[x] = ((unsigned char *)src)[x];
+		if ((unsigned char)c == ((unsigned char *)src)[x])
+			return (dest + x + 1);
+		x++;
 	}
 	return (0);
 }
